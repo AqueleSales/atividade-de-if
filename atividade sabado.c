6a1636a2d196d833d21e5603735e1c93af2d817f
AqueleSales <117695564+AqueleSales@users.noreply.github.com>
2024-08-24 18:14:30 -0300

    Add files via upload

--- atividade sabado.c
+++ atividade sabado.c
@@ -3,15 +3,15 @@
 int main(){
 
     int a, b, c, d, e, f, g;
-    printf("voce tem compromisso? S=1/N-0 ");
+    printf("voce tem compromisso? S=1/N=0 ");
     scanf("%d", &a);
-    printf("voce vai estar na cidade? S=1/N-0 ");
+    printf("voce vai estar na cidade? S=1/N=0 ");
     scanf("%d", &b);
-    printf("tem dinheiro? S=1/N-0 ");
+    printf("tem dinheiro? S=1/N=0 ");
     scanf("%d", &c);
-    printf("e perto? S=1/N-0 ");
+    printf("e perto? S=1/N=0 ");
     scanf("%d", &d);
-    printf("viu se vai chover? S=1/N-0 ");
+    printf("viu se vai chover? S=1/N=0 ");
     scanf("%d", &e);
 
     if (!a && b && c && (d || !e)) {
